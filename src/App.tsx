a82a1025ed7456b96ea9e7827fa514487521fcdf
david-cdr <150019619+david-cdr@users.noreply.github.com>
2025-03-22 11:25:39 +0200

    style(): improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -28,34 +28,38 @@ function App() {
       />
 
       <div className="flex-1 overflow-y-auto bg-gray-50">
-        <div className="max-w-4xl mx-auto p-8">
+        <div className="max-w-4xl mx-auto p-8 min-h-full flex flex-col">
           {!selectedRecording ? (
-            <div className="h-[calc(100vh-4rem)] flex flex-col items-center justify-center text-gray-400">
-              <svg
-                className="w-24 h-24 mb-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <p className="text-xl text-gray-600 mb-16">Pick a recording for analysis</p>
-              <div className="w-full max-w-2xl">
+            <>
+              <div className="flex-1 flex items-center justify-center">
+                <div className="flex flex-col items-center">
+                  <svg
+                    className="w-36 h-36 mb-6 text-indigo-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <p className="text-3xl text-indigo-400">Pick a recording for analysis</p>
+                </div>
+              </div>
+              <div className="mt-auto pb-8">
                 <EmailSignup />
               </div>
-            </div>
+            </>
           ) : (
             <div className="pb-12">
               {isAnalyzing ? (
